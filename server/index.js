f10507d45e3364a9bc6f4218663f9e40bde33731
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-12-03 17:56:17 +0530

    Mongoose set up completed

--- server/index.js
+++ server/index.js
@@ -8,3 +8,53 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+
+/* CONFIGURATIONS */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet()); //middleware for securing Express.js applications by setting various HTTP headers
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common")); // middleware for logging HTTP requests in an Express.js application
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors()); //CORS is a security feature implemented by web browsers to restrict web pages from making requests to a different domain than the one that served the original web page
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+/*Register  :  Routes with file*/
+
+app.post("/auth/register", upload.single("picture"), register);
+
+const upload = multer({ storage });
+
+/*MONGOOSE SETUP*/
+const PORT = process.env.PORT || 6001; // By default take port number from '.env' file , if that port doesn't work take port 6001
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Port ${PORT}`);
+    });
+  })
+  .catch((Error) => {
+    console.log(`${Error} did not connect`);
+  });
