8906d9da9bd95389f04b23ab25ba4364c92e821f
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2023-12-17 17:00:54 +0530

    createSlice for setMode, setLogin, setLogout, setFriends, setPosts, setPost functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
-
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import LoginPage from "scenes/loginPage";
+import HomePage from "scenes/homePage";
+import ProfilePage from "scenes/profilePage";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
